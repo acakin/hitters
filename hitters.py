ce707bc801a3cafcb1d668142b0e350148a71cf5
acakin <https://github.com/acakin>
2024-02-26 14:48:39 +0300

    Add hyperparameter optimization

--- hitters.py
+++ hitters.py
@@ -1,5 +1,3 @@
-
-
 # End-to-End Hitters Machine Learning Pipeline
 
 from helpers import *
@@ -47,14 +45,15 @@ df["SCHmRun"] = df["CHmRun"] / df["CRuns"]
 cat_cols, num_cols, cat_but_car = classify_columns_types(df)
 plot_quartiles_outlier(df, num_cols)
 for col in num_cols:
-    replace_outliers_with_thresholds(df, col, 0.05, 0.95)
+    if col != 'Salary':
+        replace_outliers_with_thresholds(df, col, 0.05, 0.95)
+
+# One-hot encoding
+df = one_hot_encoder(df, cat_cols, True)
 
 # Standardization
 num_cols = [col for col in num_cols if 'Salary' not in col]
 standardization(df, num_cols, RobustScaler())
-
-# One-hot encoding
-df = one_hot_encoder(df, cat_cols, True)
 
 # Fill NaN values of 'Salary' with KNN method
 df = pd.DataFrame(KNNImputer(n_neighbors=5).fit_transform(df), columns=df.columns)
@@ -71,16 +70,27 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 linreg = LinearRegression().fit(X_train, y_train)
 evaluate_model(linreg, X_train, y_train, X_test, y_test, "Linear Regression")
 
+
 # Lasso Regression
 lasreg = Lasso().fit(X_train,y_train)
 evaluate_model(lasreg, X_train, y_train, X_test, y_test, "Lasso Regression")
 
 # LightGBM - Light Gradient Boosting Model
-lgb_model = LGBMRegressor().fit(X_train, y_train)
+lgb_model = LGBMRegressor(force_col_wise=True).fit(X_train, y_train)
 evaluate_model(lgb_model, X_train, y_train, X_test, y_test, "Light Gradient Boosting Model")
 plot_importance(lgb_model, X_test)
+lgb_params = {"learning_rate": [0.01, 0.1],
+               "n_estimators": [500, 1000],
+               "max_depth": [3, 5, 8],
+               "colsample_bytree": [1, 0.8, 0.5]}
+tune_model(lgb_model, lgb_params, X_train, y_train, X_test, y_test, "Light Gradient Boosting Model")
 
 # Random Forest
 rf_model = RandomForestRegressor().fit(X_train, y_train)
 evaluate_model(rf_model, X_train, y_train, X_test, y_test, "Random Forest")
 plot_importance(rf_model, X_test)
+rf_params = {"max_depth": [5, 8, None],
+             "max_features": [3, 5, 15],
+             "n_estimators": [200, 500],
+             "min_samples_split": [2, 5, 8]}
+tune_model(rf_model, rf_params, X_train, y_train, X_test, y_test, "Random Forest")
